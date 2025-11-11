1bf70e88565c9f0da950e03a00ae3e97e403b6e5
zhonghong <zhonghong@orbbec.com>
2025-11-11 10:59:32 +0800

    Update Version to v2.5.5

---
+++ OrbbecSDKv2/search/enumvalues_7.js
@@ -0,0 +1,11 @@
+var searchData=
+[
+  ['start_5fof_5fexposure_0',['START_OF_EXPOSURE',['../_ob_types_8h.html#ab55581f4b14bc49ec8a4b41d5fc043e3a8d2e91847ea47578f841a1d598abf553',1,'ObTypes.h']]],
+  ['stat_5fdone_1',['STAT_DONE',['../_ob_types_8h.html#abc9ca4deac2966e28197760a66c5ed34a19b6f2f4cb4ee723bbc500cbfb6dac54',1,'ObTypes.h']]],
+  ['stat_5fdone_5fwith_5fduplicates_2',['STAT_DONE_WITH_DUPLICATES',['../_ob_types_8h.html#abc9ca4deac2966e28197760a66c5ed34ada70025bb565d4259f93da999d85bd28',1,'ObTypes.h']]],
+  ['stat_5ffile_5ftransfer_3',['STAT_FILE_TRANSFER',['../_ob_types_8h.html#abc9ca4deac2966e28197760a66c5ed34acdcc2c1211d1f69b2b01f0067e033cf9',1,'ObTypes.h']]],
+  ['stat_5fin_5fprogress_4',['STAT_IN_PROGRESS',['../_ob_types_8h.html#abc9ca4deac2966e28197760a66c5ed34a1cb56c4d921b9af93d518a63f27106a3',1,'ObTypes.h']]],
+  ['stat_5fstart_5',['STAT_START',['../_ob_types_8h.html#abc9ca4deac2966e28197760a66c5ed34abc856e15f20ef2e831eb5ce34c81080c',1,'ObTypes.h']]],
+  ['stat_5fverify_5fimage_6',['STAT_VERIFY_IMAGE',['../_ob_types_8h.html#abc9ca4deac2966e28197760a66c5ed34a4cb156866b9775624d834b8345dff730',1,'ObTypes.h']]],
+  ['stat_5fverify_5fsuccess_7',['STAT_VERIFY_SUCCESS',['../_ob_types_8h.html#abc9ca4deac2966e28197760a66c5ed34a9c2741e379cbe1713537e8e2cfaf6605',1,'ObTypes.h']]]
+];
