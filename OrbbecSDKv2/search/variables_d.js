37b157e7f8867d93fa2f7dc060a2e7c23a7a56b4
zhonghong <zhonghong@orbbec.com>
2026-01-21 22:51:50 +0800

    Update SDK version to v2.7.2

--- OrbbecSDKv2/search/variables_d.js
+++ OrbbecSDKv2/search/variables_d.js
@@ -2,5 +2,7 @@ var searchData=
 [
   ['offset_0',['offset',['../struct_o_b_data_chunk.html#ac3a32078979cac05c88b1677e8d47e0f',1,'OBDataChunk']]],
   ['offset0_1',['offset0',['../struct_o_b_disp_offset_config.html#ac35f5d706d673d8506859b4a5f33f7f4',1,'OBDispOffsetConfig']]],
-  ['offset1_2',['offset1',['../struct_o_b_disp_offset_config.html#a236b290cebac9695c0a4ec3b880eaea3',1,'OBDispOffsetConfig']]]
+  ['offset1_2',['offset1',['../struct_o_b_disp_offset_config.html#a236b290cebac9695c0a4ec3b880eaea3',1,'OBDispOffsetConfig']]],
+  ['originheight_3',['originHeight',['../struct_o_b_hardware_decimation_config.html#add5e03bda17025616d882957b9a3e6b8',1,'OBHardwareDecimationConfig']]],
+  ['originwidth_4',['originWidth',['../struct_o_b_hardware_decimation_config.html#addbc0dfd5b996033c68d314e82e35c26',1,'OBHardwareDecimationConfig']]]
 ];
