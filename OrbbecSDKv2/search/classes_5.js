a560a51af388946778138ef150452afd4df412f6
zhonghong <zhonghong@orbbec.com>
2026-04-02 19:40:26 +0800

    Update OrbbecSDKv2 version to 2.7.6

--- OrbbecSDKv2/search/classes_5.js
+++ OrbbecSDKv2/search/classes_5.js
@@ -1,10 +1,11 @@
 var searchData=
 [
-  ['filter_0',['Filter',['../classob_1_1_filter.html',1,'ob']]],
-  ['filterfactory_1',['FilterFactory',['../classob_1_1_filter_factory.html',1,'ob']]],
-  ['formatconvertfilter_2',['FormatConvertFilter',['../classob_1_1_format_convert_filter.html',1,'ob']]],
-  ['frame_3',['Frame',['../classob_1_1_frame.html',1,'ob']]],
-  ['framefactory_4',['FrameFactory',['../classob_1_1_frame_factory.html',1,'ob']]],
-  ['framehelper_5',['FrameHelper',['../classob_1_1_frame_helper.html',1,'ob']]],
-  ['frameset_6',['FrameSet',['../classob_1_1_frame_set.html',1,'ob']]]
+  ['falsepositivefilter_0',['FalsePositiveFilter',['../classob_1_1_false_positive_filter.html',1,'ob']]],
+  ['filter_1',['Filter',['../classob_1_1_filter.html',1,'ob']]],
+  ['filterfactory_2',['FilterFactory',['../classob_1_1_filter_factory.html',1,'ob']]],
+  ['formatconvertfilter_3',['FormatConvertFilter',['../classob_1_1_format_convert_filter.html',1,'ob']]],
+  ['frame_4',['Frame',['../classob_1_1_frame.html',1,'ob']]],
+  ['framefactory_5',['FrameFactory',['../classob_1_1_frame_factory.html',1,'ob']]],
+  ['framehelper_6',['FrameHelper',['../classob_1_1_frame_helper.html',1,'ob']]],
+  ['frameset_7',['FrameSet',['../classob_1_1_frame_set.html',1,'ob']]]
 ];
